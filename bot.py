204e3b5dab99c1feb1d9ba095058eb83fae06602
Artist0455 <mrsuperunboxing90@gmail.com>
2025-11-24 03:54:19 +0530

    Update BOT_TOKEN environment variable

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 # Environment variables
 API_ID = int(os.getenv('API_ID', '25136703'))
 API_HASH = os.getenv('API_HASH', 'accfaf5ecd981c67e481328515c39f89')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8521103806:AAHHQ2XL_EokOXmJCdElfkkSrnYAkr0IVB4')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8446617965:AAGRKceeAXWna3MRDf2lWOT5KLjSHktuXXI')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '8272213732'))
 
 # Initialize Flask app for health checks
