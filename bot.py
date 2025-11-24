6887a4d61d96261ab70b103518420ad96140b588
Artist0455 <mrsuperunboxing90@gmail.com>
2025-11-24 14:55:31 +0530

    Change BOT_TOKEN to a new value
    Updated BOT_TOKEN environment variable.

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 # Environment variables
 API_ID = int(os.getenv('API_ID', '25136703'))
 API_HASH = os.getenv('API_HASH', 'accfaf5ecd981c67e481328515c39f89')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8089842083:AAFmU0Jma_HNWfo7hvazxIQEw_woliuHzDE')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8366493122:AAG7nl7a3BqXd8-oyTAHovAjc7UUuLeHb-4')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '8027090675'))
 PORT = int(os.environ.get('PORT', 10000))
 
