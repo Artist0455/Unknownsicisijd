d558abc87469ae02cddd992b14e9f8b9b431c39b
Artist0455 <mrsuperunboxing90@gmail.com>
2025-11-24 05:47:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ logger = logging.getLogger(__name__)
 # Environment variables
 API_ID = int(os.getenv('API_ID', '25136703'))
 API_HASH = os.getenv('API_HASH', 'accfaf5ecd981c67e481328515c39f89')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8521103806:AAH5fhKjCyKxMOstFWaZ_YJjDP3EC1M3Yu4')
-ADMIN_ID = int(os.getenv('ADMIN_ID', '8272213732'))
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8445905664:AAEwFFZxOAQFcBfFV4_dZw20EVjZh_O9w6k')
+ADMIN_ID = int(os.getenv('ADMIN_ID', '8385462088'))
 
 # Support channels (optional join)
 SUPPORT_CHANNEL = "@shribots"
