c87a6e0882499759ffb4fa1c45becf918dec6076
Artist0455 <mrsuperunboxing90@gmail.com>
2025-11-26 19:13:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ logger = logging.getLogger(__name__)
 # Environment variables
 API_ID = int(os.getenv('API_ID', '25136703'))
 API_HASH = os.getenv('API_HASH', 'accfaf5ecd981c67e481328515c39f89')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8314503581:AAEm5TvIs_-qn23VfOCnfVL1dTRwwDtpi8A')
-ADMIN_ID = int(os.getenv('ADMIN_ID', '8272213732'))
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8244179451:AAGKYlz8J3hz_2lE41Jc8xQsLLxrArJ7e-E')
+ADMIN_ID = int(os.getenv('ADMIN_ID', '8385462088'))
 PORT = int(os.environ.get('PORT', 10000))
 
 # Import Telethon
